b09732f750a8aa11ac768ab046fd237af52eaea1
xianda31 <99276316+xianda31@users.noreply.github.com>
2023-11-01 10:12:38 +0100

    start point

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import aws_exports from './aws-exports';
+import aws_exports from '../aws-exports';
 import { Amplify } from 'aws-amplify/lib-esm';
 
 
