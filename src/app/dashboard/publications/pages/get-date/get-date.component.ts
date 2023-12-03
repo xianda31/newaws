f36d38fa94fc537c66ab6512d4273c36a29c3180
xianda31 <99276316+xianda31@users.noreply.github.com>
2023-12-03 15:01:12 +0100

    stable feature:pager (carder)

---
+++ src/app/dashboard/publications/pages/get-date/get-date.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Article } from 'src/app/API.service';
+
+@Component({
+  selector: 'app-get-date',
+  templateUrl: './get-date.component.html',
+  styleUrls: ['./get-date.component.scss']
+})
+export class GetDateComponent implements OnInit {
+  @Input() article!: Article;
+  date !: string;
+  withDate !: boolean;
+  constructor(
+    public activeModal: NgbActiveModal,
+
+  ) { }
+  ngOnInit(): void {
+    console.log('GetDateComponent', this.article);
+    this.date = this.article.info ?? '';
+    this.withDate = this.article.info ? true : false;
+
+  }
+
+
+  dateChanged() {
+    console.log('GetDateComponent : dateChanged', this.date);
+    this.article.info = this.withDate ? this.date : '';
+    this.activeModal.close(this.article);
+  }
+
+}
