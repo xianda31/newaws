72f93729cdaf94dabb4a6baf889dce91c8c9076b
xianda31 <99276316+xianda31@users.noreply.github.com>
2024-01-08 15:47:01 +0100

    reshape app.component html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { PagerComponent } from './layouts/pager/pager/pager.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CleanupComponent } from './dashboard/cleanup/cleanup.component';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
 // import { GetPictureInfoComponent } from './dashboard/publications/pages/get-picture-info/get-picture-info.component';
 
 @NgModule({
@@ -49,6 +50,7 @@ import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
     PagerComponent,
     CleanupComponent,
     DashboardComponent,
+    SidebarComponent,
 
 
 
